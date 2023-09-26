ad42174865a57e1fbee138ad36dfc018fcc8b1c6
Aki <114000157+dotcomaki@users.noreply.github.com>
2023-09-26 20:37:05 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -11,12 +11,17 @@ def generate_random_password(length=12):
 def generate_password():
     password_length = int(length_entry.get())
     random_password = generate_random_password(password_length)
-    result_label.config(text="Random Password: " + random_password)
+    
+    # Clear any existing text in the text widget
+    password_text.delete("1.0", tk.END)
+    
+    # Insert the generated password into the text widget
+    password_text.insert(tk.END, random_password)
 
 # Create the main window
 root = tk.Tk()
 root.title("Random Password Generator")
-root.geometry("300x150")
+root.geometry("300x250")
 
 # Configure style
 style = ttk.Style()
@@ -33,8 +38,8 @@ length_entry.grid(row=0, column=1, padx=10, pady=10)
 generate_button = ttk.Button(root, text="Generate Password", command=generate_password)
 generate_button.grid(row=1, columnspan=2, padx=10, pady=10)
 
-result_label = ttk.Label(root, text="")
-result_label.grid(row=2, columnspan=2, padx=10, pady=10)
+password_text = tk.Text(root, wrap=tk.WORD, height=5, width=30)
+password_text.grid(row=2, columnspan=2, padx=10, pady=10)
 
 # Start the GUI main loop
 root.mainloop()
